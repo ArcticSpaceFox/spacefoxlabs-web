850375a5ee0b7894a364da37add25da0cb0cc7a2
ArcticSpaceFox <Sm03leBr00t@protonmail.com>
2021-09-09 18:05:37 +0200

    :tada: v0.0
    set up basic page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translate(0px, 0px)' },
+          '50%' : { transform: 'translate(0px, 30px)' }
+        }
+      },
+      animation: {
+        float: 'float 5s ease-in-out infinite'
+      }
+    },
   },
   variants: {
     extend: {},
