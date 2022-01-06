b96d0b754c5c86f8b2066166a3dfbe4b07838eb4
ArcticSpaceFox <Sm03leBr00t@protonmail.com>
2022-01-06 21:08:13 +0100

    v1.1.7 cookie server side fix

--- pages/api/visited.js
+++ pages/api/visited.js
@@ -2,18 +2,18 @@ import Cookies from "cookies";
 
 export default function handler(req, res) {
   let c = new Cookies(req, res);
-  let r = Number(c.get("visited")) ?? 0;
-  r += 1;
+  let r = parseInt(c.get("visited"), 10);
 
   if (r == NaN) {
-    r = 1;
+    r = 0;
   }
+  r += 1;
 
   console.log("visited=", r);
 
   c.set("visited", r, {
     sameSite: "none",
-    secure: true,
+    secure: false,
   });
   res.status(200).json({ visited: r });
 }
